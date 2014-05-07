559661e4e4859213fec3e479e941a6fc9a6e8cfa
Yosandra Sandoval <sandoval@hlrs.de>
2014-05-07 09:45:22 +0200

    cleaning the code, remove the testing commands

--- elasticnodetest1/app.js
+++ elasticnodetest1/app.js
@@ -1,8 +1,5 @@
-
 /**
  * Module dependencies.
- //curl -i -X POST -H 'Content-Type: application/json' -d '{"Name": "MyFisrtexecution", "Description": "Testing ","other":"values","onemore":"please"}' http://localhost:3000/executions
- //curl -i -X POST -H 'Content-Type: application/json' -d '{"Name": "MyFisrtexecution", "Description": "Testing ","other":"values","onemore":"please"}' http://localhost:3000/execution/ID
  */
 
 var express = require('express');
@@ -23,7 +20,6 @@ app.set('views', path.join(__dirname, 'views'));
 //Change to html templates
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-//app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -88,7 +84,7 @@ app.get('/executions', function(req, res){
 	  })
 });
 
-
+//Show more information about the execution
 app.get('/executions/details/:ID', function(req, res){
       client.get({
             index:'executions',
@@ -107,28 +103,6 @@ app.get('/executions/details/:ID', function(req, res){
     
     })
 });
-
-
-
-//curl -i -X POST -H 'Content-Type: application/json' -d '{"Name":"Execution2","Description": "Testin filter timestamp","nodes":["node1","node2","node3"]}' http://localhost:3000/executions
-//curl -i -X POST -H 'Content-Type: application/json' -d '{"Timestamp":"1396622230","metric_1":"value1","metric_2":"value2","metric_3":"value3","metric_4":"value4"}' http://localhost:3000/execution/o0Wuji4QRCaKVwDmbVzeNQ
-//curl -i -X POST -H 'Content-Type: application/json' -d '{"Timestamp":"1396622231","metric_1":"value2","metric_2":"value2","metric_3":"value3","metric_4":"value5"}' http://localhost:3000/execution/o0Wuji4QRCaKVwDmbVzeNQ
-//curl -i -X POST -H 'Content-Type: application/json' -d '{"Timestamp":"1396622232","metric_1":"value4","metric_2":"value24","metric_3":"value34","metric_44":"value54"}' http://localhost:3000/execution/o0Wuji4QRCaKVwDmbVzeNQ
-//curl -i -X POST -H 'Content-Type: application/json' -d '{"Timestamp":"1396622233","metric_1":"value27","metric_2":"value27","metric_3":"value36","metric_4":"value56"}' http://localhost:3000/execution/o0Wuji4QRCaKVwDmbVzeNQ
-//curl -i -X POST -H 'Content-Type: application/json' -d '{"Timestamp":"1396622234","metric_1":"value2","metric_2":"value2","metric_3":"value3","metric_4":"value5"}' http://localhost:3000/execution/o0Wuji4QRCaKVwDmbVzeNQ
-//curl -i -X POST -H 'Content-Type: application/json' -d '{"Timestamp":"1396622240","metric_1":"value2","metric_2":"value2","metric_3":"value3","metric_4":"value5"}' http://localhost:3000/execution/o0Wuji4QRCaKVwDmbVzeNQ
-//curl -i -X POST -H 'Content-Type: application/json' -d '{"Timestamp":"13966222405","metric_1":"value2","metric_2":"value2","metric_3":"value3","metric_4":"value5"}' http://localhost:3000/execution/o0Wuji4QRCaKVwDmbVzeNQ
-
-//curl -i -X POST -H 'Content-Type: application/json' -d '{"Name":"Execution3","Description": "Testing filter timestamp","nodes":["node1","node2","node3"]}' http://localhost:3000/executions
-//curl -i -X POST -H 'Content-Type: application/json' -d '{"Timestamp":1396873680,"metric_1":"value1","metric_2":"value2","metric_3":"value3","metric_4":"value4"}' http://localhost:3000/execution/B5fe7pnYQzqSItnqXB3N1A
-//curl -i -X POST -H 'Content-Type: application/json' -d '{"Timestamp":1396873690,"metric_1":"value1","metric_2":"value2","metric_3":"value3","metric_4":"value4"}' http://localhost:3000/execution/B5fe7pnYQzqSItnqXB3N1A
-//curl -i -X POST -H 'Content-Type: application/json' -d '{"Timestamp":13968736905,"metric_1":"value1","metric_2":"value2","metric_3":"value3","metric_4":"value4"}' http://localhost:3000/execution/B5fe7pnYQzqSItnqXB3N1A
-//Search DB:
-//curl -XGET 'http://localhost:9200/b5fe7pnyqzqsitnqxb3n1a/_search?pretty=true'
-//curl -XGET 'http://localhost:9200/o0wuji4qrcakvwdmbvzenq/_search?pretty=true'
-//curl -i -X POST -H 'Content-Type: application/json' -d '{'Timestamp':1,'metric_1':'value1'}' http://localhost:3000/executions/geFQ3bahSE-QTHblpFJayQ
-//Browser
-//http://localhost:3000/execution/o0Wuji4QRCaKVwDmbVzeNQ/1396622231/1396622234
 
 //Searching for the values of a specific benchmark 
 app.get('/executions/:ID/:from/:to', function(req, res){
@@ -169,6 +143,7 @@ app.get('/executions/:ID/:from/:to', function(req, res){
 	  
 	  })
 });
+
 //Searching for the values of a specific benchmark
 app.get('/executions/:ID', function(req, res){
     var from_time = req.params.from;
@@ -215,7 +190,7 @@ app.post('/executions', function(req, res){
   	});
 });
 
-//Adding a new time to an existing execution and respond the provided ID //NOT WORKING
+//Adding a new time to an existing execution and respond the provided ID 
 app.post('/executions/:ID', function(req, res){
   var the_json = req.body;
 	client.index({index:req.params.ID.toLowerCase(),type: 'TBD',body:the_json},function(err,es_reply)
