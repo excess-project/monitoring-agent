f790ec0920acb5d4b1eca6bd2ed1ab9abab3e785
hpcneich <eichhorn@hlrs.de>
2014-09-01 09:13:16 +0200

    more comments

--- client/plugin_manager.h
+++ client/plugin_manager.h
@@ -16,19 +16,36 @@
 typedef metric (*PluginHook)();
 
 /**
- * @brief structure which manages the plugins
+ * @brief structure which contains the plugins
  */
 typedef struct PluginManager_t PluginManager;
 
+/**
+ * @brief initializing function for the plugin manager
+ */
 PluginManager* PluginManager_new();
 
+/**
+ * @brief functions which deallocates the memeory used
+ */
 void PluginManager_free(PluginManager *pm);
 
+/**
+ * @brief registers hooks / entry functions of plugins
+ */
 void PluginManager_register_hook(PluginManager *pm, const char *name,
 		PluginHook hook);
-
+/**
+ * @brief function which applies an entry function
+ * @deprecated { not needed }
+ */
 metric PluginManager_apply_hook(PluginManager *pm);
 
+/**
+ * @brief returns a entry function from the plugin manager structure
+ *
+ * hooks / entry functions are stored in a FIFO queue
+ */
 PluginHook PluginManager_get_hook(PluginManager *pm);
 
 #endif /* PLUGIN_MANAGER_H_ */
