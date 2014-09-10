551f925286af1e577ff21cfe6b5d47bf5fc7b504
hpcneich <eichhorn@hlrs.de>
2014-09-10 08:51:53 +0200

    changes type of numbers send in JSON message, bug fix for hostname via commandline parameter

--- client/plugins/papi.c
+++ client/plugins/papi.c
@@ -32,7 +32,7 @@ char* toPapiData(long_long *val) {
 	char *msgPart = malloc(200 * sizeof(char));
 
 	for (int i = 0; i < papiNumbers; i++) {
-		sprintf(msgPart, ",\"%s\":\"%lld\"", papiEvents[i], val[i]);
+		sprintf(msgPart, ",\"%s\":%lld", papiEvents[i], val[i]);
 		strcat(returnMsg, msgPart);
 	}
 	free(msgPart);
