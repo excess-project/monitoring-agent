636bfbf8de5340d2e64c19f80b8369d41bf155e8
Dennis Hoppe <dennis.hoppe@hlrs.de>
2014-11-25 09:26:45 +0100

    updated tests (removed tests concerning the parser)

--- client/test/test_papi.c
+++ client/test/test_papi.c
@@ -8,16 +8,14 @@
 
 #include "CuTest.h"
 #include "../plugins/papi_plugin.h"
-#include "../plugins/parser.h"
-
 
 void TestPAPI_get_named_events(CuTest *tc)
 {
-    char *events[2] = { "PAPI_TOT_INS", "PAPI_SP_OPS" };
+    char *events[1] = { "PAPI_TOT_INS"  };
     PAPI_Plugin *papi = malloc(sizeof(PAPI_Plugin));
     int num_events = sizeof(events) / sizeof(events[0]);
     read_available_named_events(papi, events, num_events);
-    CuAssertTrue(tc, papi->num_events == 2);
+    CuAssertTrue(tc, papi->num_events == 1);
 }
 
 void TestPAPI_get_ALL_PRESETS(CuTest *tc)
@@ -26,39 +24,16 @@ void TestPAPI_get_ALL_PRESETS(CuTest *tc)
     PAPI_Plugin *papi = malloc(sizeof(PAPI_Plugin));
     int num_events = sizeof(events) / sizeof(events[0]);
     read_available_named_events(papi, events, num_events);
-    CuAssertTrue(tc, papi->num_events == 52);
-}
-
-void TestPAPI_fetch_events_defined_via_conf(CuTest *tc)
-{
-    Parser *parser = get_instance();
-    read_PAPI_events_from_file(parser, "pluginConf");
-    // Events: PAPI_TOT_INS, PAPI_SP_OPS, PAPI_TOT_CYC
-
-    PAPI_Plugin *papi = malloc(sizeof(PAPI_Plugin));
-    read_available_named_events(papi, parser->events, parser->metrics_count);
-    CuAssertTrue(tc, papi->num_events == 2); // PAPI_TOT_CYC is not supported
-}
-
-void TestPAPI_fetch_preset_events_defined_via_conf(CuTest *tc)
-{
-    Parser *parser = get_instance();
-    read_PAPI_events_from_file(parser, "pluginConf_all");
-    // Events: ALL_PRESETS
-
-    PAPI_Plugin *papi = malloc(sizeof(PAPI_Plugin));
-    read_available_named_events(papi, parser->events, parser->metrics_count);
-    CuAssertTrue(tc, papi->num_events == 52);
+    CuAssertTrue(tc, papi->num_events > 40);
 }
 
 CuSuite* CuGetSuite(void)
 {
     CuSuite* suite = CuSuiteNew();
 
+    PAPI_library_init(PAPI_VER_CURRENT);
     SUITE_ADD_TEST(suite, TestPAPI_get_named_events);
     SUITE_ADD_TEST(suite, TestPAPI_get_ALL_PRESETS);
-    SUITE_ADD_TEST(suite, TestPAPI_fetch_events_defined_via_conf);
-    SUITE_ADD_TEST(suite, TestPAPI_fetch_preset_events_defined_via_conf);
 
     return suite;
 }
