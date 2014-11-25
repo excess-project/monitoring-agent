918da551009ba88d61bb78e8bc09737894fc143e
Dennis Hoppe <dennis.hoppe@hlrs.de>
2014-11-25 16:26:47 +0100

    removed redundant includes

--- client/test/test_rapl.c
+++ client/test/test_rapl.c
@@ -1,10 +1,6 @@
-#include <assert.h>
-#include <setjmp.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+
 #include <papi.h>
-#include <ctype.h>
+#include <stdlib.h>
 
 #include "../libs/CuTest/CuTest.h"
 #include "../plugins/rapl_plugin.h"
@@ -14,12 +10,6 @@ void TestRAPL_get_available_events(CuTest *tc)
     RAPL_Plugin *rapl = malloc(sizeof(RAPL_Plugin));
     int num_events = get_available_events(rapl);
     CuAssertTrue(tc, num_events == 28); // on EXCESS testbed
-
-    int i;
-    for (i = 0; i < num_events; ++i) {
-        printf("event: %s, value: %lld\n", rapl->events[i], rapl->values[i]);
-    }
-
     free(rapl);
 }
 
