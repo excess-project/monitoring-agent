5f89a3629f986b98228365e39233b92435efee8f
Dennis Hoppe <dennis.hoppe@hlrs.de>
2014-11-27 14:17:09 +0100

    added some documentation to the parser and publisher

--- client/contrib/parser/src/ini_parser.h
+++ client/contrib/parser/src/ini_parser.h
@@ -1,3 +1,14 @@
+/**
+ * @brief Parses a given file following the ini configuration format.
+ *
+ * The Parser wraps the access to the configuration file. The format of the
+ * configuration follows the INI file format.
+ *
+ * @see http://en.wikipedia.org/wiki/INI_file
+ *
+ * @author Dennis Hoppe <hoppe@hrls.de>
+ */
+
 #ifndef INI_PARSER_H_
 #define INI_PARSER_H_
 
@@ -7,12 +18,34 @@ typedef struct {
     int size;
 } mfp_data;
 
+/**
+ * @brief Parses a given file.
+ *
+ * It should be noted that each time this function is called, the internal data
+ * structure is cleared in advanced. This might currently result in some
+ * inconsistencies while accessing the data in a concurrent setting.
+ *
+ * @return 1 if successful; 0 otherwise.
+ */
 int mfp_parse(const char* filename);
 
+/**
+ * @brief Returns a stored value for the given section and key.
+ *
+ * @return the value stored for the index <section, key>
+ */
 char* mfp_get_value(const char* section, const char* key);
 
+/**
+ * @brief Sets or overwrites the value for a given section and key.
+ *
+ */
 void mfp_set_value(const char* section, const char* key, const char* value);
 
+/**
+ * @brief Returns the entire data stored for a given section.
+ *
+ */
 void mfp_get_data(const char* section, mfp_data* data);
 
 #endif
--- client/contrib/publisher/src/publisher.h
+++ client/contrib/publisher/src/publisher.h
@@ -1,3 +1,14 @@
+/**
+ * @brief Publish a message by posting it to user-defined URL
+ *
+ * The @<Publisher@> is responsible for sending data to user-defined URLs. The
+ * current implementation uses cURL to publish string messages. The monitoring
+ * framework uses during its prototyping stage the function @<publish_json@> in
+ * order to send performance and/or energy counters to the server via JSON.
+ *
+ * @author Dennis Hoppe <hoppe@hrls.de>
+ */
+
 #ifndef PUBLISHER_H_
 #define PUBLISHER_H_
 
@@ -22,12 +33,46 @@ struct Message_t {
   Data *data;
 };
 
+/**
+ * @brief Returns the execution id used for communication with Elasticsearch.
+ *
+ * If the variable #execution_id is set, then this function returns the already 
+ * initialized value of #execution_id. Otherwise, this function queries 
+ * the server via calling publish_json() based on the given URL. The #message
+ * must have the following format in JSON:
+ *
+ * \code{.c}
+ * {
+ *     "Name": <hostname>,
+ *     "Description": <description of the job>,
+ *     "Start_date": <timestamp>,
+ *     "Username": <username>
+ * }
+ * \endcode
+ */
 char* get_execution_id(const char *URL, char *message);
 
+/**
+ * @brief Publishes previously defined Message objects.
+ *
+ * It should be noted that this function is not yet implemented.
+ *
+ * @return 1
+ */
 int publish(const char *URL, Message *messages);
 
+/**
+ * @brief Sends the data defined in message to the given URL via cURL.
+ *
+ * @return 1 if successful; 0 otherwise
+ */
 int publish_json(const char *URL, char *message);
 
-void shutdown_curl(); // FIXME: CURL should be hidden
+/**
+ * @brief Frees the cURL headers and global variables.
+ *
+ * Note: This method needs to be private in order to hide cURL.
+ */
+void shutdown_curl();
 
 #endif
