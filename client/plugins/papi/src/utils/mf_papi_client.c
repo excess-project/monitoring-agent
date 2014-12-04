e2645825ac3103ce4d630c4122e292c9be865cf8
Dennis Hoppe <dennis.hoppe@hlrs.de>
2014-12-04 11:58:27 +0100

    cleaned source file and added support for multiple events passed via command line

--- client/plugins/papi/src/utils/mf_papi_client.c
+++ client/plugins/papi/src/utils/mf_papi_client.c
@@ -12,30 +12,23 @@
 
 #include "debug.h"
 
-#define LEN 64
-
 /*******************************************************************************
  * Forward Declarations
  ******************************************************************************/
-
-static int is_valid_number(char*);
 static int mf_set_affinity(int);
 int mf_PAPI_create_eventset_systemwide(int*, int, int, int, int);
 
-/*******************************************************************************
- * Main
- ******************************************************************************/
 
 int
 main(int argc, char** argv)
 {
-    long long *values;
-    char error_val[LEN];
-    int retval, ncpu, nctr, cdix, cpu_num, i;
-    int EventSet = PAPI_NULL;
-    char event_name[256];
-    int domain, granularity; 
+    int retval;
     int sleep_time;
+    int i, num_events;
+    int cdix, cpu_num;
+    int domain, granularity; 
+    int EventSet = PAPI_NULL;
+    long long *values;
 
     /***************************************************************************
      * Set default values
@@ -45,22 +38,7 @@ main(int argc, char** argv)
     cpu_num = 0;
     domain = PAPI_DOM_ALL;
     granularity = PAPI_GRN_SYS;
-    strcpy(event_name, "PAPI_L2_DCA");
     sleep_time = 1;
-
-    /***************************************************************************
-     * Read argument passed by the command line
-     **************************************************************************/
-
-    if (argc > 1 && strlen(argv[1]) > 0) {
-        strcpy(event_name, argv[1]);
-    }
-    log_info("main(int, char**) - using %s as counter", event_name);
-
-    if (argc > 2 && is_valid_number(argv[2])) {
-        sleep_time = atoi(argv[2]);
-    }
-    log_info("main(int, char**) - counting for %d seconds", sleep_time);
 
     /***************************************************************************
      * Initialize PAPI library
@@ -68,8 +46,7 @@ main(int argc, char** argv)
 
     retval = PAPI_library_init(PAPI_VER_CURRENT);
     if (retval != PAPI_VER_CURRENT) {
-        snprintf(error_val, LEN, "%d", retval);
-        log_error("main(int, char**) - PAPI_library_init: %s", error_val);
+        log_error("main(int, char**) - PAPI_library_init: %s", PAPI_strerror(retval));
     }
 
     /***************************************************************************
@@ -84,28 +61,31 @@ main(int argc, char** argv)
         granularity
     );
     if (retval != PAPI_OK) {
-        snprintf(error_val, LEN, "%d", retval);
-        log_error("main(int, char**) - PAPI_add_event: %s", error_val);
+        log_error("main(int, char**) - PAPI_add_event: %s", PAPI_strerror(retval));
     }
 
     /***************************************************************************
-     * Output initialization
+     * Add events passed via command line
      **************************************************************************/
 
-    ncpu = PAPI_get_opt(PAPI_MAX_CPUS, NULL);
-    nctr = PAPI_get_opt(PAPI_MAX_HWCTRS, NULL);
-    values = (long long *) malloc(ncpu * nctr * sizeof(long long));
-    memset(values, 0x0, (ncpu * nctr * sizeof (long long)));
-
-    /***************************************************************************
-     * Add event
-     **************************************************************************/
-
-    retval = PAPI_add_named_event(EventSet, event_name);
-    if (retval != PAPI_OK) {
-        snprintf(error_val, LEN, "%d", retval);
-        log_error("main(int, char**) - PAPI_add_event: %s", error_val);
+    for (num_events = 0, i = 1; i != argc; ++i) {
+        retval = PAPI_add_named_event(EventSet, argv[i]);
+        if (retval != PAPI_OK) {
+            log_error("main(int, char**) - PAPI_add_named_event (%s): %s",
+                argv[i], PAPI_strerror(retval));
+        } else {
+            ++num_events;
+            log_info("main(int, char**) - Added event %s", argv[i]);
+        } 
     }
+
+    if (num_events == 0) {
+        log_warn("main(int, char**) - No events added for monitoring. %s", "Abort.");
+        PAPI_shutdown();
+        exit(1);
+    }
+
+    values = malloc(num_events * sizeof(long long));
 
     /***************************************************************************
      * Start PAPI
@@ -113,8 +93,7 @@ main(int argc, char** argv)
  
     retval = PAPI_start(EventSet);
     if (retval != PAPI_OK) {
-        snprintf(error_val, LEN, "%d", retval);
-        log_error("main(int, char**) - PAPI_start: %s", error_val);
+        log_error("main(int, char**) - PAPI_start: %s", PAPI_strerror(retval));
     }
 
     sleep(sleep_time);
@@ -125,16 +104,15 @@ main(int argc, char** argv)
 
     retval = PAPI_stop(EventSet, values);
     if (retval != PAPI_OK) {
-        snprintf(error_val, LEN, "%d", retval);
-        log_error("main(int, char**) - PAPI_stop: %s", error_val);
+        log_error("main(int, char**) - PAPI_stop: %s", PAPI_strerror(retval));
     }
 
     /***************************************************************************
      * Print Counters
      **************************************************************************/
     
-    for (i = 0; i < ncpu; i++) {
-        printf("CPU%d \t %s \t%lld\n", i, event_name, values[0 + i * nctr]);
+    for (i = 0; i < num_events; i++) {
+        printf("CPU%d \t %s \t%lld\n", cpu_num, argv[i + 1], values[i]);
     }
 
     free(values);
@@ -154,23 +132,6 @@ mf_set_affinity(int thread_id)
     return retval;
 }
 
-static int
-is_valid_number(char* string)
-{
-    int i;
-
-    for(i = 0; i < strlen(string); i++) {
-        if (isdigit(string[i]) == 0) break;
-    }
-
-    if (i != strlen(string)) {
-        log_error("is_valid_number(char*) - %s is no valid integer", string);
-        return 0;
-    }
-
-    return 1;
-}
-
 int
 mf_PAPI_create_eventset_systemwide(
     int *EventSet,
@@ -180,34 +141,21 @@ mf_PAPI_create_eventset_systemwide(
     int granularity)
 {
     int retval;
-    char error_val[LEN];
     PAPI_domain_option_t domain_opt;
     PAPI_granularity_option_t gran_opt;
     PAPI_cpu_option_t cpu_opt;
 
-    retval = mf_set_affinity(cpu_num);
-    if (retval != PAPI_OK) {
-        log_error("mf_PAPI_create_eventset_systemwide() - sched_setaffinity: \
-            %s", strerror(errno));
-        return retval;
-    }
-
     retval = PAPI_create_eventset(EventSet);
     if (retval != PAPI_OK) {
-        snprintf(error_val, LEN, "%d", retval);
-        log_error("main(int, char**) - PAPI_create_eventset: %s", error_val);
+        log_error("main(int, char**) - PAPI_create_eventset: %s",
+            PAPI_strerror(retval));
         return retval;
     }
-
-    /***************************************************************************
-     * Sets some flags for per node usage
-     **************************************************************************/
 
     retval = PAPI_assign_eventset_component(*EventSet, cidx);
     if (retval != PAPI_OK) {
-        snprintf(error_val, LEN, "%d", retval);
-        log_error("main(int, char**) - PAPI_assign_eventset_component: \
-            (%s)", error_val);
+        log_error("main(int, char**) - PAPI_assign_eventset_component: (%s)", 
+            PAPI_strerror(retval));
         return retval;
     }
 
@@ -216,9 +164,8 @@ mf_PAPI_create_eventset_systemwide(
     domain_opt.domain = domain;
     retval = PAPI_set_opt(PAPI_DOMAIN, (PAPI_option_t*) &domain_opt);
     if (retval != PAPI_OK) {
-        snprintf(error_val, LEN, "%d", retval);
-        log_error("main(int, char**) - PAPI_set_opt (PAPI_DOMAIN): \
-            %s", error_val);
+        log_error("main(int, char**) - PAPI_set_opt (PAPI_DOMAIN): %s",
+            PAPI_strerror(retval));
         return retval;
     }
 
@@ -226,9 +173,8 @@ mf_PAPI_create_eventset_systemwide(
     gran_opt.granularity = granularity;
     retval = PAPI_set_opt(PAPI_GRANUL, (PAPI_option_t*) &gran_opt);
     if (retval != PAPI_OK) {
-        snprintf(error_val, LEN, "%d", retval);
-        log_error("main(int, char**) - PAPI_set_opt (PAPI_GRANUL): \
-            %s", error_val);
+        log_error("main(int, char**) - PAPI_set_opt (PAPI_GRANUL): %s",
+            PAPI_strerror(retval));
         return retval;
     }
 
@@ -236,9 +182,15 @@ mf_PAPI_create_eventset_systemwide(
     cpu_opt.cpu_num = cpu_num;
     retval = PAPI_set_opt(PAPI_CPU_ATTACH, (PAPI_option_t*) &cpu_opt);
     if (retval != PAPI_OK) {
-        snprintf(error_val, LEN, "%d", retval);
-        log_error("main(int, char**) - PAPI_set_opt (PAPI_CPU_ATTACH): \
-            %s", error_val);
+        log_error("main(int, char**) - PAPI_set_opt (PAPI_CPU_ATTACH): %s",
+            PAPI_strerror(retval));
+        return retval;
+    }
+
+    retval = mf_set_affinity(cpu_num);
+    if (retval != PAPI_OK) {
+        log_error("mf_PAPI_create_eventset_systemwide() - sched_setaffinity: %s",
+            PAPI_strerror(retval));
         return retval;
     }
 
