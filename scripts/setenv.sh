cfe69b46b92f6082f68e2ff9c6801d1af87b2dfc
Dennis Hoppe <dennis.hoppe@hlrs.de>
2015-02-17 13:27:08 +0100

    added excess cluster scripts; added make install target for excess cluster

---
+++ scripts/setenv.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+libs=/opt/mf/REV/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$libs
+export OMP_NUM_THREADS=16
---
+++ scripts/stop_mf.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#end the mf agent each of the computational nodes allocated in the pbs job
+#!warning the script will be executed under root
+
+DBKEY=$1
+NODE=$2
+PBS_JOBID=$3
+LOG_FILE=/var/log/hpcmeasure/mf_service_epilogue_${NODE}.log
+
+MF_REVISION=$(cat /opt/mf/revision)
+MF_TOP_PATH=/opt/mf/${MF_REVISION}
+MF_BIN_PATH=${MF_REVISION}/bin
+MF_SCRIPT_PATH=${MF_REVISION}/scripts
+MF_AGENT_PIDFILE_TOP_PATH=/ext/mf/plugins/${NODE}
+MF_AGENT_PIDFILE=${MF_AGENT_PIDFILE_TOP_PATH}/${PBS_JOBID}
+
+#pre-check
+DATE="$( date +'%c'  )"
+echo $DATE":--start initializing part of stop_mf.sh---" >> $LOG_FILE
+
+if [ ! -f "${MF_AGENT_PIDFILE}" ]; then
+  echo $DATE":Error in stop_mf.sh: MF agent wasn't running" >> $LOG_FILE
+fi
+
+#log details about the job
+echo $DATE":MF_AGENT_PIDFILE:"$MF_AGENT_PIDFILE >>$LOG_FILE
+echo $DATE":DBKEY:"$DBKEY >>$LOG_FILE
+
+
+#read the CONVERTER_PID
+MF_AGENT_PID="$( cat ${MF_AGENT_PIDFILE} )"
+if [ -z "${MF_AGENT_PID}" ]; then
+  echo $DATE":Error in stop_mf.sh: variable MF_AGENT_PID is not initialized" >> $LOG_FILE
+  exit 12
+fi
+
+#kill the mf agent
+if [ -f "${MF_AGENT_PIDFILE}" ]; then
+  KILL_SIGNAL_OK="$( kill -SIGINT  $MF_AGENT_PID )"
+  if [[ "${KILL_SIGNAL_OK}" -ne "0" ]]; then
+    echo $DATE":Error in stop_mf.sh: KILL_SIGNAL for mf agent is not handled" >> $LOG_FILE
+  fi
+  #echo $DATE":stop_mf.sh: The mf agent process will be stopped:"$KILL_SIGNAL_OK >> $LOG_FILE
+  #wait
+  wait $MF_AGENT_PID &>> /dev/null
+  echo $DATE":stop_mf.sh: The mf agent process is stopped" >> $LOG_FILE
+fi
+
+echo $DATE":---end ---" >> $LOG_FILE
+
+rm ${MF_AGENT_PIDFILE} -rf
+
+exit 0
