6cfecf6937ac84604251cf9b5be1cc7ea0b14da5
Dennis Hoppe <dennis.hoppe@hlrs.de>
2015-03-23 17:15:22 +0100

    iostat fixes

--- client/plugins/iostat/iostat.sh
+++ client/plugins/iostat/iostat.sh
@@ -2,4 +2,16 @@
 EXECUTION_ID=$1
 MF_SERVER=$2
 
+### check URL ###
+MF_SERVER=${MF_SERVER%/}
+URL=$MF_SERVER"/"$EXECUTION_ID
+CURL_CMD=$( curl -s -S -XGET $URL )
+echo $CURL_CMD
+if [[ "curl" == ${CURL_CMD}* ]]; then
+    exit 1
+fi
+case "$CURL_CMD" in
+  *Cannot*     ) exit 1 ;;
+esac
+
 iostat -m 1 | awk -v server=$2 -v id=$1 -f iostat.awk
