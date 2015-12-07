4441e3c83b2667336896fc7230e74b0cc4d097ef
Dennis Hoppe <dennis.hoppe@hlrs.de>
2015-12-07 15:51:37 +0100

    added README to rapl plugin

--- client/plugins/rapl/setenv.sh
+++ client/plugins/rapl/setenv.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## Copyright 2014,2015 University of Stuttgart
+## Authors: Dennis Hoppe
 PWD=`pwd`
 export LD_LIBRARY_PATH=$PWD/../../../binaries/papi/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$PWD/../../lib:$LD_LIBRARY_PATH
