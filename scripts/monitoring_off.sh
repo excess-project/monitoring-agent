b78d95e065cbfc46f1a8a7877e25fbed6cd7cadb
Dennis Hoppe <dennis.hoppe@hlrs.de>
2015-12-17 11:51:49 +0100

    renamed shell scripts in scripts folder

--- scripts/monitoring_off.sh
+++ scripts/monitoring_off.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "Deactivating monitoring framework"
+rm -f $HOME/.mf/service/node03/start_monitoring
 rm -f $HOME/.mf/service/node02/start_monitoring
 rm -f $HOME/.mf/service/node01/start_monitoring
