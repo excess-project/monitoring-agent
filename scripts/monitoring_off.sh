76e83f9f3816eb02635934377496a1ea6bb2e5ae
Dennis Hoppe <dennis.hoppe@hlrs.de>
2015-12-17 13:49:36 +0100

    delete cluster-specific shell scripts

--- scripts/monitoring_off.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Deactivating monitoring framework"
-rm -f $HOME/.mf/service/node03/start_monitoring
-rm -f $HOME/.mf/service/node02/start_monitoring
-rm -f $HOME/.mf/service/node01/start_monitoring
--- scripts/monitoring_on.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Activating monitoring framework"
-mkdir -p $HOME/.mf/service/node03
-mkdir -p $HOME/.mf/service/node02
-mkdir -p $HOME/.mf/service/node01
-touch $HOME/.mf/service/node03/start_monitoring
-touch $HOME/.mf/service/node02/start_monitoring
-touch $HOME/.mf/service/node01/start_monitoring
