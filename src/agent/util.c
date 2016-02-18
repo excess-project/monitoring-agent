62c1eb59ab9fd38603e8ed50654fc0bbf2dedd07
Dennis Hoppe <dennis.hoppe@hlrs.de>
2016-02-18 17:12:27 +0100

    cleaned util.c

--- src/agent/util.c
+++ src/agent/util.c
@@ -22,17 +22,13 @@
 #include "excess_main.h"
 #include "util.h"
 
-int foo(int bar) {
-    return 1;
-}
-
 int getFQDN(char *fqdn) {
     struct addrinfo hints, *info, *p;
 
     int gai_result;
 
-    char *hostname = malloc(sizeof(char) * 80);
-    gethostname(hostname, sizeof hostname);
+    char hostname[1024];
+    gethostname(hostname, 1024);
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
@@ -40,24 +36,29 @@ int getFQDN(char *fqdn) {
     hints.ai_flags = AI_CANONNAME;
 
     if ((gai_result = getaddrinfo(hostname, "http", &hints, &info)) != 1) {
-        //fprintf(stderr, "getaddrinfo: %s,\n using regular hostname\n",
-        //        gai_strerror(gai_result));
         FILE *tmp = NULL;
         if ((tmp = popen("hostname", "r")) == NULL ) {
             perror("popen");
             return -1;
         }
         char line[200];
-        while (fgets(line, 200, tmp) != NULL )
+        while (fgets(line, 200, tmp) != NULL ) {
             sprintf(fqdn, "%s", line);
+        }
+        fclose(tmp);
+
         return 1;
     }
     for (p = info; p != NULL ; p = p->ai_next) {
         sprintf(fqdn, "%s\n", p->ai_canonname);
     }
 
-    if (info->ai_canonname)
+    if (info->ai_canonname) {
         freeaddrinfo(info);
+    }
+
+    free(info);
+    free(p);
 
     return 1;
 }
